de5fb88c7e9fd3ef1731b653657c787031a775a0
Kazuya Hatano <k28@me.com>
2021-02-23 15:22:07 +0900

    Fix rubocop command to pass.

--- example/get_current_temp.rb
+++ example/get_current_temp.rb
@@ -1,42 +1,43 @@
+# frozen_string_literal: true
 require "ondotori-ruby-client"
 
 WEB_STORAGE_ACCESS_INFO_PATH = "/var/tmp/webstorage.json"
-=begin
-The webstorage.json looks like the following.
-{
-    "api-key":"T&D WebStorage API Key",
-    "login-id" : "rbacxxxx",
-    "login-pass" : "password"
-}
-=end
+# The webstorage.json looks like the following.
+# {
+#     "api-key":"T&D WebStorage API Key",
+#     "login-id" : "rbacxxxx",
+#     "login-pass" : "password"
+# }
 
+# Reads the information for API access from the file.
 def load_params
-  begin
-    File.open(WEB_STORAGE_ACCESS_INFO_PATH) do |file|
-      storage_info = file.read
-      load_info = JSON.parse(storage_info)
+  File.open(WEB_STORAGE_ACCESS_INFO_PATH) do |file|
+    storage_info = file.read
+    load_info = JSON.parse(storage_info)
 
-      wss_access_info = {}
-      wss_access_info["api-key"]    = load_info["api-key"]
-      wss_access_info["login-id"]   = load_info["login-id"]
-      wss_access_info["login-pass"] = load_info["login-pass"]
-      return wss_access_info
-    end
-  rescue SystemCallError => e
-    puts %(class=[#{e.class}] message=[#{e.message}])
-  rescue IOError => e
-    puts %(class=[#{e.class}] message=[#{e.message}])
+    # This is not necessary, but is left for the explanation of the parameter settings.
+    wss_access_info = {}
+    wss_access_info["api-key"]    = load_info["api-key"]
+    wss_access_info["login-id"]   = load_info["login-id"]
+    wss_access_info["login-pass"] = load_info["login-pass"]
+    return wss_access_info
   end
+rescue SystemCallError => e
+  puts %(class=[#{e.class}] message=[#{e.message}])
+rescue IOError => e
+  puts %(class=[#{e.class}] message=[#{e.message}])
 end
 
 def main
-  unless params = load_params
+  params = load_params
+  if params.nil?
     puts "Load parameter error..."
+    return
   end
 
   begin
     client = Ondotori::WebAPI::Client.new(params)
-    response = client.current()
+    response = client.current
     puts "#{response}"
   rescue Ondotori::WebAPI::Api::Errors::Error => e
     puts "Some error happend #{e.message} #{e.code}"
@@ -44,4 +45,3 @@ def main
 end
 
 main
-
